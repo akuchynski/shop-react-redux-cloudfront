b3408f5cadfb0a5cba527530dfdecfe304b0b87e
Andrei Kuchynski <Andrei_Kuchynski1@epam.com>
2022-10-05 13:08:10 +0300

    update file import component

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://zvrqc8geel.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://zvrqc8geel.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://zvrqc8geel.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://08w86xdqy8.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://zvrqc8geel.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://zvrqc8geel.execute-api.eu-central-1.amazonaws.com/dev",
 };
